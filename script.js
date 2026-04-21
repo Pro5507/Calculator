a0f3ac685ac27c6df292d8d48eed327aabb7d9a0
Pro5507 <nhupendrakumar9162@gmail.com>
2026-04-21 21:36:57 +0530

    Calculator

--- script.js
+++ script.js
@@ -15,3 +15,56 @@ function printOutput(num){
         document.getElementById("output-value").innerText=getFormattedNumber(num);
     }
 }
+function getFormattedNumber(num){
+    if(num='-'){
+        return "";
+    }
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("operator");
+for(var i = 0; i<operator.length; i++){
+    if(this.id=="clear"){
+        printHistory("");
+        printOutput("");
+    }
+    else if(this.id=="backspace"){
+        var output=reverseNumberFormat(getOutput()).toString();
+        if(output){
+            output= output.substr(0,output.length-1);
+            printOutput(output);
+        }
+    }
+    else{
+        var output=getOutput();
+        var history=getHistory();
+        if(output==""&&history!=""){
+            output= output==""?output:reverseNumberFormat(output);
+            history=history+output;
+            if(this.id=="="){
+                var result=eval(history);
+                printOutput(result);
+                printHistory("");
+            }
+            else{
+                history=history+this.id;
+                printHistory(history);
+                printOutput("");
+            }
+        }
+    }
+}
+var number = document.getElementsByClassName("number");
+for(var i = 0; i<number.length; i++){
+    number[i].addEventListener('click', function(){
+        var output=reverseNumberFormat(getOutput());
+        if(output!=NaN){
+            output=output+this.id;
+            printOutput(output);
+        }
+    });
+}
